b66396b9cc9f5b4af1ac903d166a2474221c6934
Wen-Tien Chang <ihower@gmail.com>
2017-03-14 00:39:15 +0800

    Install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, :defaults => { :format => :json } do
@@ -21,4 +22,5 @@ Rails.application.routes.draw do
   	end
   end
   
+  root "welcome#index"
 end
