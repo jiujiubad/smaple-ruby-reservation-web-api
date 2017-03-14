c077b6d1a212c231a98901c60b46c5149745160e
Wen-Tien Chang <ihower@gmail.com>
2017-03-14 11:46:21 +0800

    Add signup, login, logout API

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   namespace :api, :defaults => { :format => :json } do
   	namespace :v1 do
+
+      post "/signup" => "auth#signup"
+      post "/login" => "auth#login"
+      post "/logout" => "auth#logout"
+
       #resources :trains, :only => [:index, :show]
       get "/trains"  => "trains#index", :as => :trains
       get "/trains/:train_number" => "trains#show", :as => :train
