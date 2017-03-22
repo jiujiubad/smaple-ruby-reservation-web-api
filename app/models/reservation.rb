8d8bb920b03a2580b96d1875bed29f23d0fc42cd
Wen-Tien Chang <ihower@gmail.com>
2017-03-22 16:01:03 +0800

    Test reservation api

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,7 +3,7 @@ class Reservation < ApplicationRecord
 	validates_presence_of :train_id, :seat_number, :booking_code
 	validates_uniqueness_of :seat_number, :scope => :train_id
 
-	belongs_to :user
+	belongs_to :user, :optional => true
 	belongs_to :train
 
 	before_validation :generate_booking_code, :on => :create
